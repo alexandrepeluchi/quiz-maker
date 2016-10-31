55c8b685415576509deb50fe38b82e6050ad2de6
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2016-10-31 10:24:53 -0200

    Creating a class to interactions in the database

---
+++ Study Quiz/Study Quiz/DB.vb
@@ -0,0 +1,29 @@
+﻿Imports MySql.Data.MySqlClient
+
+Public Class DB
+    Private comando As New MySqlCommand
+    Private armazena As MySqlDataReader
+
+    Public Sub AbrirConexao(ByRef conexao As MySqlConnection)
+        conexao = New MySqlConnection
+        conexao.ConnectionString = "server=localhost; user id=root; password= ; database=Quiz"
+        conexao.Open()
+    End Sub
+
+    Public Sub FecharConexao(ByRef conexao As MySqlConnection)
+        conexao.Close()
+    End Sub
+
+    Public Sub ExecutarSQL(ByVal conexao As MySqlConnection, ByVal instrucao As String)
+        comando.Connection = conexao
+        comando.CommandText = instrucao
+        comando.ExecuteNonQuery()
+    End Sub
+
+    Public Function ExecutarSelect(ByVal conexao As MySqlConnection, ByVal instrucao As String) As MySqlDataReader
+        comando.Connection = conexao
+        comando.CommandText = instrucao
+        armazena = comando.ExecuteReader()
+        Return armazena
+    End Function
+End Class
