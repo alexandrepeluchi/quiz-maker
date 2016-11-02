2ff4e41b130e152e26fb85b4594d3d3ca24c367b
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2016-11-02 20:49:23 -0200

    Some changes to select a quiz theme

--- Study Quiz/Study Quiz/EscolhaMateria.Designer.vb
+++ Study Quiz/Study Quiz/EscolhaMateria.Designer.vb
@@ -24,7 +24,9 @@ Partial Class EscolhaMateria
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.btnVoltar = New System.Windows.Forms.Button()
-        Me.btnComecar = New System.Windows.Forms.Button()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnAC = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
@@ -38,32 +40,43 @@ Partial Class EscolhaMateria
         '
         'btnVoltar
         '
-        Me.btnVoltar.Location = New System.Drawing.Point(76, 348)
+        Me.btnVoltar.Location = New System.Drawing.Point(160, 358)
         Me.btnVoltar.Name = "btnVoltar"
         Me.btnVoltar.Size = New System.Drawing.Size(75, 23)
         Me.btnVoltar.TabIndex = 1
         Me.btnVoltar.Text = "Voltar"
         Me.btnVoltar.UseVisualStyleBackColor = True
         '
-        'btnComecar
+        'Panel1
         '
-        Me.btnComecar.Location = New System.Drawing.Point(250, 348)
-        Me.btnComecar.Name = "btnComecar"
-        Me.btnComecar.Size = New System.Drawing.Size(75, 23)
-        Me.btnComecar.TabIndex = 2
-        Me.btnComecar.Text = "Começar"
-        Me.btnComecar.UseVisualStyleBackColor = True
+        Me.Panel1.AutoScroll = True
+        Me.Panel1.AutoScrollMargin = New System.Drawing.Size(1, 1)
+        Me.Panel1.Controls.Add(Me.btnAC)
+        Me.Panel1.Location = New System.Drawing.Point(87, 79)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(227, 263)
+        Me.Panel1.TabIndex = 3
+        '
+        'btnAC
+        '
+        Me.btnAC.Location = New System.Drawing.Point(23, 17)
+        Me.btnAC.Name = "btnAC"
+        Me.btnAC.Size = New System.Drawing.Size(179, 23)
+        Me.btnAC.TabIndex = 0
+        Me.btnAC.Text = "Arquitetura de Computadores"
+        Me.btnAC.UseVisualStyleBackColor = True
         '
         'EscolhaMateria
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(393, 431)
-        Me.Controls.Add(Me.btnComecar)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.btnVoltar)
         Me.Controls.Add(Me.Label1)
         Me.Name = "EscolhaMateria"
         Me.Text = "EscolhaMateria"
+        Me.Panel1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -71,5 +84,6 @@ Partial Class EscolhaMateria
 
     Friend WithEvents Label1 As Label
     Friend WithEvents btnVoltar As Button
-    Friend WithEvents btnComecar As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents btnAC As Button
 End Class
--- Study Quiz/Study Quiz/EscolhaMateria.vb
+++ Study Quiz/Study Quiz/EscolhaMateria.vb
@@ -1,6 +1,23 @@
 ﻿Public Class EscolhaMateria
+    Dim pergunta As Perguntas
+
     Private Sub btnVoltar_Click(sender As Object, e As EventArgs) Handles btnVoltar.Click
         Inicio.Show()
         Me.Close()
+    End Sub
+
+    Private Sub btnAC_Click(sender As Object, e As EventArgs) Handles btnAC.Click
+        Dim resultado
+        resultado = MessageBox.Show("Iniciar Quiz apartir da matéria, Arquitetura de Computadores?", "Atenção!", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+        If (resultado = vbYes) Then
+            abrirFechar()
+            pergunta.SelecionaMateria(1)
+        End If
+
+    End Sub
+
+    Private Sub abrirFechar()
+        Perguntas.Show()
+        Me.Hide()
     End Sub
 End Class
